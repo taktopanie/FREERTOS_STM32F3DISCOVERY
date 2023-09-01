10e0f6154a86733842fdf6d2bc2feb6d37aaa88b
Taktopanie <maciejzwolan@gmail.com>
2023-09-01 16:15:16 +0200

    Task deletion project, will be updated with suspension

---
+++ LED_BUTTON_task_notify/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
@@ -0,0 +1,6 @@
+common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
+ /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
+ /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
+ /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
+/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
+/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
