3797eb360142d03de1a447a271a7abbf5670bebc
taktopanie <maciejzwolan@gmail.com>
2023-10-21 20:29:09 +0200

    Debug folder deleted

--- HelloWorld/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- HelloWorld/Debug/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- ../ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/HelloWorld/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
