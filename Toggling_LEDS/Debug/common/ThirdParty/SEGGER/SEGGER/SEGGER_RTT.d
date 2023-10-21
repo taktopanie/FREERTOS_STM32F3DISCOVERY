20a7419a9027ed651cade7fab01a278215afd359
taktopanie <maciejzwolan@gmail.com>
2023-10-21 18:33:55 +0200

    Debug folder deleted

--- Toggling_LEDS/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-/home/taktopanie/Desktop/moje/programming/STM32/RTOS_workspace/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
